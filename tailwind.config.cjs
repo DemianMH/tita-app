d0a9b12113330bc3dea9cb6bf9c60afbcd5d9e78
Demian <za19011250@zapopan.tecmm.edu.mx>
2024-12-05 18:25:13 -0600

    Primer commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Incluye todos los archivos en la carpeta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
